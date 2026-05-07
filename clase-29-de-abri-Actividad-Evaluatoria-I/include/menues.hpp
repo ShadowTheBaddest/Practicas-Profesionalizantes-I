c21fc620a1b0ee09627dc577678ea50e511b739f
Uriel Nicolas Sommariva <141687256+ShadowTheBaddest@users.noreply.github.com>
2026-05-07 20:03:39 -0300

    Add files via upload

---
+++ clase-29-de-abri-Actividad-Evaluatoria-I/include/menues.hpp
@@ -0,0 +1,8 @@
+#ifndef MENUES_HPP
+#define MENUES_HPP
+
+void mostrarMenuPrincipal();
+void mostrarMenuOperacionesBasicas();
+void mostrarMenuOperacionesVariadas();
+
+#endif // MENUES_HPP
---
+++ clase-29-de-abri-Actividad-Evaluatoria-I/main.cpp
@@ -0,0 +1,9 @@
+#include "menues.hpp"
+#include "consola.hpp"
+#include "operaciones.hpp"
+
+int main()
+{
+    mostrarMenuPrincipal();
+    return 0;
+}
